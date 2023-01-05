a254c6167b16e34c7ad0107564f5ecb53937e563
Rodolfo Capelline <rodolfocapelline@gmail.com>
2023-01-05 12:14:32 -0300

    fixed name of route

--- app/api/Controllers/BooksController.cs
+++ app/api/Controllers/BooksController.cs
@@ -6,7 +6,7 @@ using api.Services;
 namespace api.Controllers;
 
 [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/books")]
     public class BooksController : ControllerBase
     {
         private readonly BooksService _booksService;
